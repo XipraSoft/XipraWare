159e791cd57f1a4866df84bbd5fc9264ba775b2f
Muhammad Usman Raza <usmansipra264@gmail.com>
2025-08-18 03:10:42 -0700

    cleaning code

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,10 +1,9 @@
-// middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+  const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) return res.status(401).json({ message: 'Access token missing' });
 
